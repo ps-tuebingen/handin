ce0320f4e76f02155cbc7ada28561cfca1fccdeb
Paolo G. Giarrusso <p.giarrusso@gmail.com>
2015-10-09 12:19:53 +0200

    Move checker utilities from 00-starting checker

--- handin-server/checker-extras.rkt
+++ handin-server/checker-extras.rkt
@@ -1,21 +1,49 @@
 #lang racket
+(require handin-server/checker)
 (provide (all-defined-out))
 
-;; Checker utilities to save state across runs of the checker.
+(define user-error-message
+  (string-append "We cannot run your code.\n\n"
+                  "Please fix the error before submitting.\n"
+                  "Hit \"Run\" and debug your code.\n\n"
+                  "This is the error we got:\n\n"
+                  "~a\n"))
 
-; The checker is run in a per-submission directory, so we go up to access a state shared across submissions.
-; This is inside $handin/$user.
-(define status-path (string->path "../status"))
+(define saved-grades (make-hash))
+(define (start-grading)
+  ; Copy handin.rkt to Korrektur.rkt
+  (void))
 
-(define (write-to value path)
-  (call-with-output-file path
-    (lambda (out) (write value out))
-    #:exists 'truncate))
-(define (read-from path)
-  (call-with-input-file path (lambda (in) (read in))))
+(define GRADE-TEMPLATE-PATH "../../grade-template.rktd")
+; Save grades
+(define (end-grading)
+  (with-output-to-file
+      "grade.rktd"
+    (λ () (display (for/fold ([grade (port->string (open-input-file GRADE-TEMPLATE-PATH))])
+                             ([(item-name item-grade) saved-grades])
+                     (string-replace grade item-name (number->string item-grade)))))))
 
-(define (read-status-internal) (read-from status-path))
-(define (write-status value) (write-to value status-path))
+; Save item-grade as grade for item-name
+(define (set-grade item-name item-grade)
+  (hash-set! saved-grades item-name item-grade))
 
-(define (read-status)
-  (if (file-exists? status-path) (read-status-internal) 'init))
+(define (error* fmt . args)
+  (message (apply format fmt args) '(ok caution)))
+
+(define (handle-not-found symbol)
+  (lambda (e)
+    (error* (string-append "We cannot find the constant '~a'.\n\n"
+                           "Remember to program your expression like this:\n\n"
+                           "  (define ~a\n"
+                           "    (... your expression here ...))\n\n"
+                           "This is the error we got:\n\n"
+                           "~a\n")
+            symbol symbol
+            (exn-message e))))
+
+(define-syntax-rule (extract-from-handin name body ...)
+  (let ([name
+         (with-handlers
+             ([exn:fail? (handle-not-found 'name)])
+           (!eval name))])
+    body ...))
