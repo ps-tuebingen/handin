27a9cfdf93f5e81425478e83545dd3a9c7ff183b
Paolo G. Giarrusso <p.giarrusso@gmail.com>
2015-10-29 17:34:54 +0100

    Always show a warning icon
    Most messages that checkers send, in particular all that current/future
    checkers send, clearly call for a warning icon because they're
    about (potential) bugs.
    
    The first checker(s) also have positive messages, but "warning" might
    still mean "please pay attention to this message".
    
    We could also allow the checker to specify which kind of warning it
    should use, but I believe that'd require a protocol change, so let's not
    do that.

--- handin-client/client-gui.rkt
+++ handin-client/client-gui.rkt
@@ -177,8 +177,8 @@
       (queue-callback
        (lambda ()
          (when abort-commit-dialog (send abort-commit-dialog show #f))
-         ; Using `ok` below is not necessarily right, we should get info from the client.
-         (message-box "Handin" final-message this '(ok))
+         ; Using `'(ok caution)` below is not necessarily right, we should get info from the client.
+         (message-box "Handin" final-message this '(ok caution))
          ; This is hacky; final-message has multiple lines,
          ; but only the first will be shown.
          ; You better start your success message with "Handin saved"
