29e5feda456440234f48aadee25d5a4fcf45b1bd
mm0821 <mari.mueller@uni-tuebingen.de>
2025-10-15 21:25:37 +0200

    Disable certificate verification completely

--- handin-client/client.rkt
+++ handin-client/client.rkt
@@ -35,12 +35,11 @@
 (define (connect-to server port [cert #f])
   ;(define pem (or cert (in-this-collection "server-cert.pem")))
   (define ctx (ssl-make-client-context))
-  (ssl-try-verify! ctx #t)
+  (ssl-set-verify! ctx #f)
   (ssl-load-default-verify-sources! ctx)
   ;(if (file-exists? pem)
   ;    (ssl-load-verify-root-certificates! ctx pem)
   ;    (ssl-set-verify-hostname! ctx #t))
-  (ssl-set-verify-hostname! ctx #t)
   (with-handlers
       ([exn:fail:network?
         (lambda (e)
